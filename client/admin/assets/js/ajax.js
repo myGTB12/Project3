ff4110723d2fee1745ab7718f8ab0d0bf0cd6bd6
tungb <tungbo121@gmail.com>
2023-02-15 22:59:03 +0700

    update client admin

--- client/admin/assets/js/ajax.js
+++ client/admin/assets/js/ajax.js
@@ -1,9 +1,35 @@
-function buyToken() {
+window.onload = function () {
+  getdata()
+}
+function getdata() {
   $.ajax({
     type: 'GET',
-    url: 'http://localhost:3000/buyToken',
+    url: 'http://localhost:3000/project-detail',
     success: function (data) {
-      console.log(data)
+      document.getElementById('token').value = data.tokenAddress
+      document.getElementById('firstRelease').value = data.firstRelease
+      document.getElementById('startTime').value = data.startTime
+      document.getElementById('cliff').value = data.cliff
+      document.getElementById('totalPeriods').value = data.totalPeriods
+      document.getElementById('timePerPeriods').value = data.timePerPeriods
+      document.getElementById('projectName').innerHTML =
+        'Project name: ' + data.projectName
+      if (data.totalTokens == 0) {
+        document.getElementById('totalTokens').value = data.totalTokens
+      } else {
+        document.getElementById('totalTokens').value = data.totalTokens
+        document.getElementById('totalTokens').readOnly = true
+      }
+    },
+  })
+}
+
+function setTransaction() {
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:3000/transactions',
+    success: function (transaction) {
+      document.getElementById()
     },
   })
 }
@@ -17,6 +43,18 @@ $('#form-create-pj').submit(function (e) {
     data: $(this).serialize(),
     success: function (data) {
       console.log(data)
+    },
+  })
+})
+
+$('#form-vesting-pj').submit(function (e) {
+  e.preventDefault()
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:3000/fundVesting',
+    data: $(this).serialize(),
+    success: function (data) {
+      console.log('data ' + data)
     },
   })
 })
